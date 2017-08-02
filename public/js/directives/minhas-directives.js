2186bd650a29a34a2682aaefbee90c1bebe64c76
Raul Pereira <raul.pereira@gmail.com>
2017-08-02 02:11:03 -0300

    Aula 6 - Dividir para conquistar (bônus)

--- public/js/directives/minhas-directives.js
+++ public/js/directives/minhas-directives.js
@@ -11,4 +11,17 @@ angular.module('minhasDirectives', [])
     ddo.templateUrl = 'js/directives/meu-painel.html';
 
     return ddo;
+  })
+  .directive('minhaFoto', function() {
+
+    var ddo = {};
+
+    ddo.restrict = "AE";
+    ddo.scope = {
+      titulo: '@',
+      url: '@'
+    };
+    ddo.template = '<img class="img-responsive center-block" ng-src="{{url}}" alt="{{titulo}}">';           
+
+    return ddo;
   });
