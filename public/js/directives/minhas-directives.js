5415b6fdc5dd43ee42dd7ebdd6783d034015bc63
Raul Pereira <raul.pereira@gmail.com>
2017-08-02 02:55:02 -0300

    Aula 7 - Precisamos alimentar esse sistema

--- public/js/directives/minhas-directives.js
+++ public/js/directives/minhas-directives.js
@@ -2,26 +2,24 @@ angular.module('minhasDirectives', [])
   .directive('meuPainel', function() {
 
     var ddo = {};
-
     ddo.restric = "AE";
     ddo.scope = {
       titulo: '@'
     };
     ddo.transclude = true;
     ddo.templateUrl = 'js/directives/meu-painel.html';
-
     return ddo;
+
   })
   .directive('minhaFoto', function() {
 
     var ddo = {};
-
     ddo.restrict = "AE";
     ddo.scope = {
       titulo: '@',
       url: '@'
     };
     ddo.template = '<img class="img-responsive center-block" ng-src="{{url}}" alt="{{titulo}}">';           
-
     return ddo;
+
   });
