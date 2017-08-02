efd87b89855dde416ef7e4bb3efb529f772c7472
Raul Pereira <raul.pereira@gmail.com>
2017-08-02 03:21:20 -0300

    Aula 8 - Dados inconsistentes? Precisamos removê-los o alterá-los

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,20 +1,42 @@
 angular.module('alurapic')
-  .controller('FotoController', function($scope, $http) {
+  .controller('FotoController', function($scope, $http, $routeParams) {
 
     $scope.foto = {};
     $scope.mensagem = '';
 
+    if($routeParams.fotoId) {
+      $http.get('v1/fotos/' + $routeParams.fotoId)
+        .success(function(foto) {
+          $scope.foto = foto;
+        })
+        .error(function(erro) {
+          console.log(erro);
+          $scope.mensagem = 'Não foi possível obter a foto de ID ' + $routeParams.fotoId;
+        });
+    }
+
     $scope.submeter = function() {
       if ($scope.formulario.$valid) {
-        $http.post('v1/fotos', $scope.foto)
-          .success(function() {
-            $scope.foto = {};
-            $scope.mensagem = 'Foto cadastrada com sucesso!';
-          })
-          .error(function(erro) {
-            console.log(erro);
-            $scope.mensagem = 'Não foi possível cadastrar a foto!';
-          });
+        if($routeParams.fotoId) {
+          $http.put('v1/fotos/' + $routeParams.fotoId, $scope.foto)
+            .success(function() {
+              $scope.mensagem = 'Foto editada com sucesso!';
+            })
+            .error(function(erro) {
+              console.log(erro);
+              $scope.mensagem = 'Não foi possível editar a foto!';
+            });
+        } else {
+          $http.post('v1/fotos', $scope.foto)
+            .success(function() {
+              $scope.foto = {};
+              $scope.mensagem = 'Foto cadastrada com sucesso!';
+            })
+            .error(function(erro) {
+              console.log(erro);
+              $scope.mensagem = 'Não foi possível cadastrar a foto!';
+            });
+        }
       }
     };
 
--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -3,6 +3,7 @@ angular.module('alurapic')
 
     $scope.filtro = "";
     $scope.fotos = [];
+    $scope.mensagem = '';
 
     $http.get('v1/fotos')
       .success(function(fotos) {
@@ -11,5 +12,18 @@ angular.module('alurapic')
       .error(function(erro) {
         console.log(erro);
       });
+
+    $scope.remover = function(foto) {
+      $http.delete('v1/fotos/' + foto._id)
+        .success(function() {
+          var indexFoto = $scope.fotos.indexOf(foto);
+          $scope.fotos.splice(indexFoto, 1);
+          $scope.mensagem = 'Foto ' + foto.titulo + ' foi removida com sucesso!';
+        })
+        .error(function(erro) {
+          console.log(erro);
+          $scope.mensagem = 'Não foi possível remover a foto ' + foto.titulo;
+        });
+    };
 
   });
