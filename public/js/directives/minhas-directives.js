2203328c4fc52130479429a9654fa8234093a37d
Raul Pereira <raul.pereira@gmail.com>
2017-08-06 08:36:25 -0300

    Aula 12 - Componentizar Ainda é o Melhor Investimento: Precisamos Saber Mais Sobre Diretivas! (bônus 1)

--- public/js/directives/minhas-directives.js
+++ public/js/directives/minhas-directives.js
@@ -46,4 +46,19 @@ angular.module('minhasDirectives', [])
     };
     return ddo;
 
+  })
+  .directive('meusTitulos', function() {
+
+    var ddo = {};
+    ddo.restric = 'E';
+    ddo.template = '<ul><li ng-repeat="titulo in titulos">{{titulo}}</li></ul>';
+    ddo.controller = function($scope, recursoFoto) {
+      recursoFoto.query(function(fotos) {
+        $scope.titulos = fotos.map(function(foto) {
+          return foto.titulo;
+        });
+      });
+    };
+    return ddo;
+
   });
